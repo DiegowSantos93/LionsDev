ea3b67763338797dabfdc7a270e92982ee41280a
Diego Santos (5531) <diego.santos@quiver.local>
2024-06-26 16:11:31 -0300

    finalizacao exercicio

--- cadastro_medico/index.js
+++ cadastro_medico/index.js
@@ -12,10 +12,10 @@ while (true) {
     5. Sair
     `);
 
-    let opcao = prompt("Escolha uma opção: ");
+    let opcao = Number(prompt("Escolha uma opção: "));
 
     switch (opcao) {
-        case '1':
+        case 1:
             let nome = prompt('Qual o nome do paciente? ')
             let medico = prompt('Qual o nome do médico que gostaria de marcar a consulta? ')
             let data = prompt('Qual a data da consulta? ')
@@ -23,7 +23,7 @@ while (true) {
             consultas.push({nome, medico, data, horario})
             console.log('Consulta agendada com sucesso!')
             break;
-        case '2':
+        case 2:
             if (consultas.length <= 0){
                 console.log('Nenhuma consulta agendada. Tente novamente.')
             } else {
@@ -33,39 +33,63 @@ while (true) {
                 });
             }
             break;
-        case '3':
+        case 3:
             console.log(`Estas são as consultas agendadas:`)
             consultas.forEach((consultas, index) => {
                 console.log(`${index + 1}. Paciente: ${consultas.nome} - Médico: Dr. ${consultas.medico} - Data da consulta: ${consultas.data} - Horário da consulta: ${consultas.horario}.`)
             });
-            let edicao = prompt('Digite o número da consulta que deseja alterar: ')
-                if (edicao > consultas.length){
-                    console.log('Opção inválida, tente novamente.')
-                } else {
-                    consultas.forEach((consultas, index) => {
-                        if (edicao == index + 1){
-                            console.log(`Essa é a consulta selecionada: ${index + 1}. Paciente: ${consultas.nome} - Médico: Dr. ${consultas.medico} - Data da consulta: ${consultas.data} - Horário da consulta: ${consultas.horario}.`)
-                            }
-                                let edicaoDados = prompt('Digite o número da informação que deseja editar:\n1. Nome do paciente\n2. Médico responsável\n3. Data da consulta\n4. Horário da consulta ');
-                                if (edicaoDados == '1') {
-                                console.log(edicaoDados)
 
-                            //     let edicaoNome = prompt('Qual o nome do paciente? ');
-                            //     consultas.splice(edicao[index + 1], 1, edicaoNome);
-                            //     console.log(consultas)
-                            // }
-                        } 
+            let edicao = Number(prompt('Digite o número da consulta que deseja alterar: '));
+
+            if (edicao > 0 && edicao <= consultas.length) {
+                let consultaSelecionada = consultas[edicao - 1];
+                let edicaoDados = Number(prompt(`Qual informação deseja alterar na consulta do(a) ${consultaSelecionada.nome}? Digite o número da opção desejada:\n1. Nome do paciente\n2. Médico responsável\n3. Data da consulta\n4. Horário da consulta `));                
+                
+                switch (edicaoDados) {
+                    case 1:
+                        let novoNome = prompt('Qual o nome do paciente? ');
+                        consultaSelecionada.nome = novoNome
+                        break;
+                    case 2:
+                        let novoMedico = prompt('Qual o novo médico responsável? ');
+                        consultaSelecionada.medico = novoMedico;
+                        break;
+                    case 3:
+                        let novaData = prompt('Qual a nova data da consulta? ');
+                        consultaSelecionada.data = novaData;
+                        break;
+                    case 4:
+                        let novoHorario = prompt('Qual o novo horário da consulta? ');
+                        consultaSelecionada.horario = novoHorario;
+                        break;
+                    default:
+                        console.log('Opção inválida.');
+                        break;
+                }
+                
+            console.log(`Consulta alterada com sucesso! Estas são as consultas agendadas:`)
+                consultas.forEach((consultas, index) => {
+                console.log(`${index + 1}. Paciente: ${consultas.nome} - Médico: Dr. ${consultas.medico} - Data da consulta: ${consultas.data} - Horário da consulta: ${consultas.horario}.`)
+            });
+            } else {
+                console.log('Número inválido, tente novamente.');
+            }
+            break;
+        case 4:
+            console.log(`Estas são as consultas agendadas:`)
+            consultas.forEach((consultas, index) => {
+                console.log(`${index + 1}. Paciente: ${consultas.nome} - Médico: Dr. ${consultas.medico} - Data da consulta: ${consultas.data} - Horário da consulta: ${consultas.horario}.`)
+            });
+            let delecao = Number(prompt('Digite o número da consulta que deseja cancelar: '));
+                if (delecao > 0 && delecao <= consultas.length){
+                    consultas.splice(delecao - 1, 1);
+                    console.log(`Consulta desmarcada com sucesso! Estas são as consultas restantes e que seguem agendadas:`)
+                        consultas.forEach((consultas, index) => {
+                        console.log(`${index + 1}. Paciente: ${consultas.nome} - Médico: Dr. ${consultas.medico} - Data da consulta: ${consultas.data} - Horário da consulta: ${consultas.horario}.`)
                     });
                 }
             break;
-        case '4':
-            console.log(`Estas são as consultas agendadas:`)
-            consultas.forEach((consultas, index) => {
-                console.log(`${index + 1}. Paciente: ${consultas.nome} - Médico: Dr. ${consultas.medico} - Data da consulta: ${consultas.data} - Horário da consulta: ${consultas.horario}.`)
-            });
-            let delecao = prompt('Digite o número da consulta que deseja cancelar: ')
-            break;
-        case '5':
+        case 5:
             console.log('Saindo do sistema, até logo!')
             process.exit();
     }
