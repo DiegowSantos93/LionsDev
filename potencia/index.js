3cdb7a6a2cccd7de733ffce411208cfe11bb5379
DiegowSantos93 <diegowillian41@gmail.com>
2024-06-26 22:22:03 -0300

    Ajuste codigo

--- potencia/index.js
+++ potencia/index.js
@@ -3,13 +3,15 @@ const prompt = require("prompt-sync")({ sigint: true });
 let numero = +prompt('Digite um número: ');
 let expoente = +prompt('Digite o expoente: ');
 
-let resultado = 0
-let numeroMultiplicado = numero
+let resultado = 1; // Inicializa com 1, pois qualquer número elevado a 0 é 1
+let numeroMultiplicado = numero;
 
-for (let x = 0; x < expoente; x++) {
+for (let x = 1; x <= expoente; x++) {
+    let acumulador = 0;
     for (let j = 0; j < numero; j++) {
-        resultado += numeroMultiplicado
+        acumulador += numeroMultiplicado;
     }
-    numeroMultiplicado = resultado
+    numeroMultiplicado = acumulador;
 }
-    console.log(`${resultado}`)
+
+console.log(`Resultado de ${numero} elevado a ${expoente} é: ${numeroMultiplicado}`);
